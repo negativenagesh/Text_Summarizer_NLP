86eab043e824cba76d774f78db53337b552abb19
Subrahmanya Gaonkar <gaonkarsub@gmail.com>
2024-07-09 00:38:20 +0530

    modified template.py

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = 'Text_Summarizer_NLP'
+project_name = 'textsummarizer'
 
 list_of_files = [
     ".github/workflows/.gitkeep",
