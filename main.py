cdc96df4006598b2094bcd81891112ad890169d4
Subrahmanya Gaonkar <gaonkarsub@gmail.com>
2024-07-09 13:16:14 +0530

    modified main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from Text_Summarizer_NLP.logging import logger
+from src.textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.textsummarizer.logging import logger
 
-logger.info("Welcome to my custom logging")
-
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} ended <<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
