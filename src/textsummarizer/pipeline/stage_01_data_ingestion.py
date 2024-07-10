a1fb1d63ef92aac098e8519bcb0184ba28f399b5
Subrahmanya Gaonkar <gaonkarsub@gmail.com>
2024-07-10 09:01:58 +0530

    Data Ingestion notebook added

---
+++ src/textsummarizer/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,15 @@
+from src.textsummarizer.config.configuration import ConfigurationManager
+from src.textsummarizer.components.data_ingestion import DataIngestion
+from src.textsummarizer.logging import logger
+
+
+class DataIngestionTrainingPipeline(DataIngestion):
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
