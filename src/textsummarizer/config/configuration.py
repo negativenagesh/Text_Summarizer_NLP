9ce4f7c66dd119f9a2c4eade56ecdfc28c91ac9e
Subrahmanya Gaonkar <gaonkarsub@gmail.com>
2024-07-10 12:15:30 +0530

    modified src/textsummarizer/config/configuration.py

--- src/textsummarizer/config/configuration.py
+++ src/textsummarizer/config/configuration.py
@@ -1,6 +1,6 @@
 from src.textsummarizer.constants import *
 from src.textsummarizer.utils.common import read_yaml, create_directories
-from src.textsummarizer.entity import (DataIngestionConfig)
+from src.textsummarizer.entity import (DataIngestionConfig,DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH):
@@ -22,3 +22,16 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
