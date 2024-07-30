792f1e91574ff6d7f58b46c15da265ad9f589aed
Subrahmanya Gaonkar <gaonkarsub@gmail.com>
2024-07-30 21:33:00 +0530

    datasets

--- src/textsummarizer/pipeline/stage_01_data_ingestion.py
+++ src/textsummarizer/pipeline/stage_01_data_ingestion.py
@@ -13,3 +13,5 @@ class DataIngestionTrainingPipeline:
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
+
+    
