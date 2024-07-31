0ec725626e4c3cbab771d533d8bda2aa8b8ea5d0
Subrahmanya Gaonkar <gaonkarsub@gmail.com>
2024-07-31 17:00:40 +0530

    added datatransformationconfig

--- src/textsummarizer/config/configuration.py
+++ src/textsummarizer/config/configuration.py
@@ -1,6 +1,6 @@
 from src.textsummarizer.constants import *
 from src.textsummarizer.utils.common import read_yaml, create_directories
-from src.textsummarizer.entity import (DataIngestionConfig,DataValidationConfig)
+from src.textsummarizer.entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig)
 
 class ConfigurationManager:
     def __init__(self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH):
@@ -35,3 +35,15 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+    
+        create_directories([config.root_dir])
+    
+        data_transformation_config=DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name=config.tokenizer_name)
+        
+        return data_transformation_config
